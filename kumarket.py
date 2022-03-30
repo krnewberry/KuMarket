ecb8127e0006515eb8473ad3ebf5fc30a26b5148
k` <kylenewberry@gmail.com>
2022-03-30 11:06:00 -0400

    v1.0

--- kumarket.py
+++ kumarket.py
@@ -3,11 +3,10 @@ import pandas as pd
 import time
 from datetime import datetime, timezone
 
-# GET MARKET PAIRINGS FROM KUCOIN /api/v1/symbols
+# GET all market pairs on Kucoin via /api/v1/symbols
 
 # KUCOIN API
 api_url = "https://api.kucoin.com"
-api_rate_limit = 1
 csv_name = "Kucoin_Market-Pairs"
 
 # API CALL
@@ -16,7 +15,23 @@ data = requests.get(f"{api_url}/api/v1/symbols",
     
 # CSV EXPORT   
 df = pd.DataFrame(data.json()["data"],
-    columns = ["symbol","name","baseCurrency","quoteCurrency","market","baseMinSize","quoteMinSize","baseMaxSize","quoteMaxSize","baseIncrement","quoteIncrement","priceIncrement","feeCurrency","enableTrading","isMarginEnabled","priceLimitRate"])
-df = df[["symbol","name","market","enableTrading"]]
+    columns = ['symbol',
+    'name',
+    'baseCurrency',
+    'quoteCurrency',
+    'market',
+    'baseMinSize',
+    'quoteMinSize',
+    'baseMaxSize',
+    'quoteMaxSize',
+    'baseIncrement',
+    'quoteIncrement',
+    'priceIncrement',
+    'feeCurrency',
+    'enableTrading',
+    'isMarginEnabled',
+    'priceLimitRate'])
+
+# df = df[["symbol","name","market"]]
 df.sort_values(by='symbol', inplace=True)
-df.to_csv(csv_name+'.csv', mode='a', header=True, index=False)
+df.to_csv(csv_name+'.csv', header=True, index=False)
